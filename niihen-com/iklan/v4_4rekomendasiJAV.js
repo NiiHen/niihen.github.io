574d6f1315e6f296742662f70a8b646175c8892a
NiiHen <63357664+NiiHen@users.noreply.github.com>
2022-10-22 00:26:56 +0700

    Update and rename v3_4rekomendasiJAV.js to v4_4rekomendasiJAV.js

--- niihen-com/iklan/v4_4rekomendasiJAV.js
+++ niihen-com/iklan/v4_4rekomendasiJAV.js
@@ -8,14 +8,14 @@ console.log("Ada 4rekomendasiJAV");
       }
       jQuery(function($) {
         $("#rss-feeds").rss("https://www.blogger.com/feeds/7486822895993461897/posts/default?max-results=4&start-index=1", {
-          entryTemplate: "<ul class='ms-related-hasthumb'>"+
+          entryTemplate: "<li>"+
 		
-	"<li><a rel='dofollow' href='" + "{url}" + 
+	"<a rel='dofollow' href='" + "{url}" + 
         "'><div class='related-thumb-outer'><img class='lazyload related-thumb shown' data-src='" + "{teaserImageUrl}" + 
         "' alt='Download JAV " + "{title}" + 
         " ' title='Download JAV " + "{title}" + 
         " ' src='https://lh4.googleusercontent.com/-8FaUdtZGvFM/XZzJNW-VG5I/AAAAAAAADIo/5520jWvYEqUQ_D8i_vva369pfT1iRtrhwCLcBGAsYHQ/w300-h170-rw-n-l85-fSoften=10,30,0/NiiHen.png' width='192' height='108'/></div><div class='related-title-outer'>"+"{title}"+
-        "</div></a></li>"+			
+        "</div></a>"+			
 						//	"<div id='url'>{url}</div>"+
 						//	"<div id='author'>{author}</div>"+
 						//	"<div id='date'>{date}</div>"+
@@ -28,7 +28,7 @@ console.log("Ada 4rekomendasiJAV");
 						//	"<div id='index'>{index}</div>"+
 						//	"<div id='totalEntries'>{totalEntries}</div>"+
 						//	"<div id='body'>{body}</div>"+
-			 "</ul>", 
+			 "</li>", 
         support: false, 
 	limit: 100,
         tokens: {
